e3a86c7ef2d883dfe5e556b4645b3ae6dd992ae4
Syrine Belakaria <syrineb@sc.stanford.edu>
2025-07-14 20:35:53 -0700

    formatting

--- README.md
+++ README.md
@@ -2,15 +2,47 @@
 ## Multi-Fidelity Multi-Objective Bayesian Optimization: An Output Space Entropy Search Approach
 
 
+---
 
-### Requirements
-The code is implemented in Python and requires the following packages:
+## Requirements
 
-1. [platypus](https://platypus.readthedocs.io/en/latest/getting-started.html#installing-platypus)
+This code is implemented in Python and requires the following dependencies:
 
-2. [sklearn.gaussian_process](https://scikit-learn.org/stable/modules/gaussian_process.html)
+* [`sobol_seq`](https://github.com/naught101/sobol_seq) – for generating Sobol sequences
+* [`platypus`](https://platypus.readthedocs.io/en/latest/getting-started.html#installing-platypus) – for multi-objective evolutionary algorithms
+* [`scikit-learn`](https://scikit-learn.org/stable/modules/gaussian_process.html) – specifically `sklearn.gaussian_process` for GP modeling
+* [`pygmo`](https://esa.github.io/pygmo2/install.html) – for parallel optimization algorithms
 
-3. [pygmo](https://esa.github.io/pygmo2/install.html) 
+You can install the required packages using:
+
+```bash
+pip install sobol_seq platypus-opt scikit-learn pygmo
+```
+---
+## Running MF-OSEMO
+
+
+```bash
+python main.py <function_names> <d> <seed> <initial_number> <total_iterations> <sample_number> <approximation>
+```
+
+Here's an example command you could run from bash:
+
+```bash
+python main.py branin,Currin 2 0 5 100 10 TG
+```
+
+Explanation of arguments:
+
+1. `function_names`: names of the benchmark functions separated by a comma
+2. `d`: number of input dimensions 
+3. `seed`: random seed 
+4. `initial_number`: number of initial of evaluations
+5. `total_iterations`: number of BO iterations
+6. `sample_number`: number of samples to use for entropy estimation 
+7.  `approximation`: approximation used for the acquisition function TG or NI
+
+---
 
 ### Citation
 If you use this code in your academic work please cite our papers:
--- test_functions.py
+++ test_functions.py
@@ -1,44 +1,115 @@
 import math
-import numpy as np
-from scipy.interpolate import interp1d
 from copy import deepcopy
+
+import numpy as np
+
+
+def get_benchmark_functions(names="branin,Currin"):
+    """
+    Returns a list of benchmark functions.
+    """
+    names = names.split(",")
+    available_functions = {
+        "branin": mfbranin,
+        "Currin": mfCurrin,
+        # Add other functions here as needed
+    }
+    functions = []
+    for name in names:
+        if name in available_functions:
+            functions.append(available_functions[name])
+        else:
+            raise ValueError(f"Function '{name}' is not available.")
+    return functions
+
+
+def get_cost(names="branin,Currin"):
+    """
+    Returns a list of cost functions.
+    """
+    names = names.split(",")
+    available_cost_functions = {
+        "branin": [1, 10],
+        "Currin": [1, 10],
+        # Add other cost functions here as needed
+    }
+    cost_functions = []
+    for name in names:
+        if name in available_cost_functions:
+            cost_functions.append(available_cost_functions[name])
+        else:
+            raise ValueError(f"Cost function '{name}' is not available.")
+    return cost_functions
 
 
 ########################################################
 def Currin(x1, d):
-    x=deepcopy(x1)
-    if x[1]==0:
-        x[1]=1e-100
-    return -1*float(((1 - math.exp(-0.5*(1/x[1]))) * ((2300*pow(x[0],3) + 1900*x[0]*x[0] + 2092*x[0] + 60)/(100*pow(x[0],3) + 500*x[0]*x[0] + 4*x[0] + 20))))
-def lowFideliltyCurrin(x,d):
+    x = deepcopy(x1)
+    if x[1] == 0:
+        x[1] = 1e-100
+    return -1 * float(
+        (
+            (1 - math.exp(-0.5 * (1 / x[1])))
+            * (
+                (2300 * pow(x[0], 3) + 1900 * x[0] * x[0] + 2092 * x[0] + 60)
+                / (100 * pow(x[0], 3) + 500 * x[0] * x[0] + 4 * x[0] + 20)
+            )
+        )
+    )
+
+
+def lowFideliltyCurrin(x, d):
     a = [x[0] + 0.05, x[1] + 0.05]
     b = [x[0] + 0.05, max(0, x[1] - 0.05)]
     c = [x[0] - 0.05, x[1] + 0.05]
     f = [x[0] - 0.05, max(0, x[1] - 0.05)]
     ### we do not multply by -1 because it will come from Currin
-    return 0.25 * (Currin(a,d) + Currin(b,d)+ Currin(c,d) + Currin(f,d))
-    
-def mfCurrin(x,d,f):
-    if f==1:
-        return Currin(x,d)
-    elif f==0:
-        return lowFideliltyCurrin(x,d)
+    return 0.25 * (Currin(a, d) + Currin(b, d) + Currin(c, d) + Currin(f, d))
+
+
+def mfCurrin(x, d, f):
+    if f == 1:
+        return Currin(x, d)
+    elif f == 0:
+        return lowFideliltyCurrin(x, d)
+
+
 ##################################################################3
-def branin(x1,d):
-    x=deepcopy(x1)
-    x[0]= 15* x[0]-5
-    x[1]=15*x[1]
-    return -1*float(np.square(x[1] - (5.1/(4*np.square(math.pi)))*np.square(x[0]) + (5/math.pi)*x[0]- 6) + 10*(1-(1./(8*math.pi)))*np.cos(x[0]) + 10)
-def lowFideliltybranin(x1,d):
-    x=deepcopy(x1)
-    x[0]= 15* x[0]-5
-    x[1]=15*x[1]
-    first_term = (x[1] - (5.1/(4*pow(np.pi,2)) -0.01)*x[0]**2+ (5/np.pi -0.1)*x[0] - 6)**2
-    second_term = 10*(1 - 1./(8*np.pi) - 0.05 )*np.cos(x[0]) + 10
-    return - (first_term + second_term) # + (x[0]+5)/3
-def mfbranin(x1,d,f):
-    if f==1:
-        return branin(x1,d)
-    elif f==0:
-        return lowFideliltybranin(x1,d)
+def branin(x1, d):
+    x = deepcopy(x1)
+    x[0] = 15 * x[0] - 5
+    x[1] = 15 * x[1]
+    return -1 * float(
+        np.square(
+            x[1]
+            - (5.1 / (4 * np.square(math.pi))) * np.square(x[0])
+            + (5 / math.pi) * x[0]
+            - 6
+        )
+        + 10 * (1 - (1.0 / (8 * math.pi))) * np.cos(x[0])
+        + 10
+    )
+
+
+def lowFideliltybranin(x1, d):
+    x = deepcopy(x1)
+    x[0] = 15 * x[0] - 5
+    x[1] = 15 * x[1]
+    first_term = (
+        x[1]
+        - (5.1 / (4 * pow(np.pi, 2)) - 0.01) * x[0] ** 2
+        + (5 / np.pi - 0.1) * x[0]
+        - 6
+    ) ** 2
+    second_term = 10 * (1 - 1.0 / (8 * np.pi) - 0.05) * np.cos(x[0]) + 10
+    return -(first_term + second_term)  # + (x[0]+5)/3
+
+
+def mfbranin(x1, d, f):
+    if f == 1:
+        return branin(x1, d)
+    elif f == 0:
+        return lowFideliltybranin(x1, d)
+
+
 ##############################################################
